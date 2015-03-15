b103e7aed7f29c869fd4b3b8b732ab3cd84164f5
Christiaan Rossouw <cbrossouw@gmail.com>
2015-03-15 07:46:53 +0000

    sendgrid emails working

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,10 +2,10 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :address               =>  'smtp.sendgrid.net',
   :port                  =>  '587',
-  :authentication        =>  :plain,
-  :user                  =>  'app34826814@heroku.com',
+  :user_name                 =>  'app34826814@heroku.com',
   :password              =>  'h9b8p8qk',
   :domain                =>  'heroku.com',
+  :authentication        =>  :plain,
   :enable_starttls_auto  =>  true
   }
   
