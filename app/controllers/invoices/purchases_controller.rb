c1b7e33873bffdf0a4dd73df443375b074d89b62
Christiaan Rossouw <cbrossouw@gmail.com>
2015-03-19 10:56:13 +0000

    added line item and invoice totals

--- app/controllers/invoices/purchases_controller.rb
+++ app/controllers/invoices/purchases_controller.rb
@@ -60,6 +60,6 @@ class Invoices::PurchasesController < ApplicationController
     end
 
     def purchase_params
-      params.require(:purchase).permit(:name, :category, :quantity, :invoice_id)
+      params.require(:purchase).permit(:name, :category, :quantity, :invoice_id, :price)
     end
 end
